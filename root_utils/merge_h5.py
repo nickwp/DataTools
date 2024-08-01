bf5397b935b93310d2079505f38bdc3dd03e909c
Nick Prouse <n.prouse@imperial.ac.uk>
2024-08-01 14:20:19 +0100

    fixes for merge_h5 to make it more robust and faster

--- root_utils/merge_h5.py
+++ root_utils/merge_h5.py
@@ -12,42 +12,47 @@ def get_args():
 if __name__ == '__main__':
     config = get_args()
     print("output file:", config.output_file)
-    out_file = h5py.File(config.output_file, 'w')
-    infiles = [h5py.File(f, 'r') for f in config.input_files]
-    print(f"opened input file {infiles[0].filename}")
-    keys = infiles[0].keys()
-    attr_keys = infiles[0].attrs.keys()
-    for f in infiles[1:]:
+    out_file = h5py.File(config.output_file, 'w', track_order=True)
+    first_file = h5py.File(config.input_files[0], 'r')
+    print(f"opened and checking input file {first_file.filename}")
+    keys = first_file.keys()
+    attr_keys = first_file.attrs.keys()
+    attrs = {k: v if isinstance(v, list) else [v] for k, v in first_file.attrs.items()}
+    shapes = {k: list(first_file[k].shape) for k in keys}
+    dtypes = {k: first_file[k].dtype for k in keys}
+    print(type(first_file.attrs['command']), len(first_file.attrs['command']))
+    for fn in config.input_files[1:]:
+        f = h5py.File(fn, 'r')
         print(f"opened and checking input file {f.filename}")
         if f.keys() != keys:
             raise KeyError(f"HDF5 file {f.filename} keys {f.keys()} do not match first file's keys {keys}.")
         if f.attrs.keys() != attr_keys:
             raise KeyError(f"HDF5 file {f.filename} attributes {f.attrs.keys()} do not match first file's attributes {attr_keys}.")
-    for k in attr_keys:
-        out_file.attrs[k]  = np.hstack([f.attrs[k] for f in infiles]).tolist()
-    for k in keys:
-        dtype = infiles[0][k].dtype
-        shape = list(infiles[0][k].shape)
-        for f in infiles[1:]:
-            shape[0] += f[k].shape[0]
-            if shape[1:] != list(f[k].shape[1:]):
+        for k in attr_keys:
+            attrs[k].extend(f.attrs[k])
+        for k in keys:
+            shapes[k][0] += f[k].shape[0]
+            if shapes[k][1:] != list(f[k].shape[1:]):
                 raise ValueError(f"Array {k} in {f.filename} has shape {f[k].shape} which is incompatible with extending previous files shape {shape}.")
-        print(f"writing {k}, shape {shape}, dtype {dtype}")
-        dset = out_file.create_dataset(k, shape=shape, dtype=dtype)
-        isIndex = False
-        if k == "event_hits_index":
-            isIndex = True
-            offset = 0
-            print("  is an index array, so adding length of hit_pmt array in each file to the index values of the following file")
-        start = 0
-        for f in infiles:
-            stop = start+f[k].shape[0]
-            print(f"  entries {start}:{stop} from file {f.filename}")
+    for k in attr_keys:
+        print(f"Storing attribute {k}.")
+        out_file.attrs[k]  = attrs[k]
+    dsets = {k: out_file.create_dataset(k, shape=shapes[k], dtype=dtypes[k]) for k in keys}
+    starts = {k: 0 for k in keys}
+    offset = 0
+    for fn in config.input_files:
+        print(f"Opening {fn}")
+        f = h5py.File(fn, 'r')
+        for k in keys:
+            stop = starts[k] + f[k].shape[0]
+            isIndex = (k == "event_hits_index")
+            print(f"   writing {dtypes[k]} {k} entries {starts[k]} to {stop}" +
+                    (f" adding {offset} offest to the values for index array" if isIndex else ""))
             if isIndex:
-                dset[start:stop] = np.array(f[k]) + offset
+                dsets[k][starts[k]:stop] = np.array(f[k]) + offset
                 offset += f['hit_pmt'].shape[0]
             else:
-                dset[start:stop] = f[k]
-            start = stop
+                dsets[k][starts[k]:stop] = f[k]
+            starts[k] = stop
     print(f"Written output file {out_file.filename}.")
     out_file.close()
